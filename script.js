b2457a7526af257e4fd8828ef8835c3ff1b601c5
996573526@qq.com <996573526@qq.com>
2021-08-13 10:17:53 +0800

    提交文件

---
+++ script.js
@@ -0,0 +1,3 @@
+// Dear Monday, would you like some rainbow sprinkles?
+
+// Currently this script is hosted somewhere else because I used a structure (with ES6 imports) that can't be placed here. Soon I'll update my pens with github sources!
